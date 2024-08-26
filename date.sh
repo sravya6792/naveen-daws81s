ccbbcabcbf06b6fe274cbf6191a1cf1f25c126ae
naveen <naveenari117@gmail.com>
2024-08-26 18:10:15 +0530

    date

--- date.sh
+++ date.sh
@@ -1,7 +1,9 @@
 userid=$(id -u)
-if[ $ userid -ne 0 ]
+if [ $userid -ne 0 ]
 then
-    echo "please run this script with root"
+   echo"please run the script with root"
+   exit 1
 
 
 fi
+dnf install git -y
