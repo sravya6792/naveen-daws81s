28e3556bc25871c6edd77aa32acbfd9654873a0c
naveen <naveenari117@gmail.com>
2024-08-27 17:46:15 +0530

    date

---
+++ date5.sh
@@ -0,0 +1,6 @@
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+echo "hello" &>> $LOG_FILE
