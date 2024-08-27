3b5122e9468be2176d6eda8b1a6293eb070d2220
naveen <naveenari117@gmail.com>
2024-08-27 15:59:18 +0530

    date

--- date1.sh
+++ date1.sh
@@ -4,15 +4,18 @@ R="\e[31m"
 G="\e[32m"
 N="\e[33m"
 
-
+LOGS_FOLDER="/var/log/shell_script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date + %y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.LOG"
 
 validate()
 {
     if [ $? -ne 0 ]
     then
-        echo  -e "$2....$R is not installed $N"
+        echo  -e "$2....$R is not installed $N"&>>LOG_FILE
     else
-        echo  -e "$2...$G is installed  $N"
+        echo  -e "$2...$G is installed  $N"&>>LOG_FILE
 
     fi        
 }
@@ -21,17 +24,17 @@ validate()
 echo "$userdata"
 if [ $userdata -ne 0 ]
 then
-   echo "plz choose root access"
+   echo "plz choose root access"&>>LOG_FILE
    exit 1
 else
-   echo "git installed"
+   echo "git installed"&>>LOG_FILE
 fi
        dnf list installed gitttt
        
        
 if [ $? -ne 0 ]
 then
-   echo "git is not installed plz install"
+   echo "git is not installed plz install"&>>LOG_FILE
    dnf install gittt
    
    
@@ -40,19 +43,19 @@ then
    validate $? "git is"
    
 else
-   echo "git is already installed"
+   echo "git is already installed"&>>LOG_FILE
 fi
  dnf list avaialable mysql 
        
        
 if [ $? -ne 0 ]
 then
-   echo "mysql is not installed plz install"
+   echo "mysql is not installed plz install"&>>LOG_FILE
    dnf install mysql -y
    validate $? "mysql is"
    
 else
-   echo "mysql is already installed"
+   echo "mysql is already installed"&>>LOG_FILE
 fi
 
 
