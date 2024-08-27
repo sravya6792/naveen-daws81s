36cc5d6acb49b468bd9fae2741071a22f9aec6b7
naveen <naveenari117@gmail.com>
2024-08-27 20:59:23 +0530

    date

--- date7.sh
+++ date7.sh
@@ -7,6 +7,8 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+
+
 mkdir -p $LOGS_FOLDER
 
 
@@ -39,7 +41,7 @@ do
 if [ $? -ne 0 ]
 then
    echo "$package is not installed plz install"&>>$LOG_FILE
-   dnf install $package&>>LOG_FILE
+   dnf install $package -y&>>LOG_FILE
    
    
    
