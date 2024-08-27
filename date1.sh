be457ed0d527a089ff5e5dfedef013fcfcde5f04
naveen <naveenari117@gmail.com>
2024-08-27 10:16:40 +0530

    date

--- date1.sh
+++ date1.sh
@@ -4,9 +4,10 @@ userdata=$(id -u)
 echo "$userdata"
 if [ $userdata -ne 0 ]
 then
-   echo "hi"
+   echo "plz choose root access"
+   exit 1
 else
    echo "hello"
 fi
-       
+       dnf install git -y
 
