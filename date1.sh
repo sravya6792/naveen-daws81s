17f63968a9a46633f1f4d372802610af2ed1a9fc
naveen <naveenari117@gmail.com>
2024-08-27 16:04:56 +0530

    date

--- date1.sh
+++ date1.sh
@@ -8,6 +8,7 @@ LOGS_FOLDER="/var/log/shell_script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.LOG"
+mkdir -p $LOGS_FOLDER
 
 validate()
 {
