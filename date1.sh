232d335c39953d005b9f73ae26da0ba46bbb52aa
naveen <naveenari117@gmail.com>
2024-08-27 10:23:35 +0530

    date

--- date1.sh
+++ date1.sh
@@ -7,7 +7,7 @@ then
    echo "plz choose root access"
    exit 1
 else
-   echo "hello"
+   echo "git installed"
 fi
        dnf install git -y
        
@@ -15,6 +15,11 @@ if [ $? -ne 0 ]
 then
    echo "git is not installed plz install"
    dnf install git -y
+   if [ $? -ne 0 ]
+   then
+      echo "git is not installed"
+   else 
+      echo "git is installed boss"
 else
    echo "git is already installed"
 fi
