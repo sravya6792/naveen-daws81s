199336043ee6c6a87e0bf2ad3c5b06962b5fe3e3
naveen <naveenari117@gmail.com>
2024-08-27 20:47:42 +0530

    date

---
+++ date7.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+userdata=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
+
+
+echo "hi"
+
+
+validate()
+{
+    if [ $? -ne 0 ]
+    then
+        echo  -e "$2....$R is not installed $N"| tee -a $LOG_FILE
+    else
+        echo  -e "$2...$G is installed  $N"| tee -a $LOG_FILE
+
+    fi        
+}
+echo "$userdata"
+if [ $userdata -ne 0 ]
+then
+   echo "plz choose root access"|tee -a $LOG_FILE
+   exit 1
+else
+   echo "git installed"&>>LOG_FILE
+fi
+
