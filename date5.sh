4ed9b25262491197a15370de9c7c7f0ec1785f81
naveen <naveenari117@gmail.com>
2024-08-27 17:49:11 +0530

    date

--- date5.sh
+++ date5.sh
@@ -1,6 +1,60 @@
-LOGS_FOLDER="/var/log/shell-script"
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
-echo "hello" &>> $LOG_FILE
+#!/bin/bash
+userdata=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+
+
+
+validate()
+{
+    if [ $? -ne 0 ]
+    then
+        echo  -e "$2....$R is not installed $N"
+    else
+        echo  -e "$2...$G is installed  $N"
+
+    fi        
+}
+
+
+echo "$userdata"
+if [ $userdata -ne 0 ]
+then
+   echo "plz choose root access"
+   exit 1
+else
+   echo "git installed"
+fi
+       dnf list installed gitttt
+       
+       
+if [ $? -ne 0 ]
+then
+   echo "git is not installed plz install"
+   dnf install gittt
+   
+   
+   
+    -y
+   validate $? "git is"
+   
+else
+   echo "git is already installed"
+fi
+ dnf list avaialable mysql 
+       
+       
+if [ $? -ne 0 ]
+then
+   echo "mysql is not installed plz install"
+   dnf install mysql -y
+   validate $? "mysql is"
+   
+else
+   echo "mysql is already installed"
+fi
+
+
+
+
