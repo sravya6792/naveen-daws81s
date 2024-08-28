ac197519cd44d4cab34d3d7e6591a13d4be4cc5d
naveen <naveenari117@gmail.com>
2024-08-28 11:02:06 +0530

    conditions

--- nav.sh
+++ nav.sh
@@ -1,8 +1,16 @@
-#!/bin/bash
-LOGS_FOLDER="var/log/shell_script"
-SCRIPT_NAME=$($0 |cut -d "." -f1)
-TIMESTAMP=$(date +%H-%m-%d-%M-%H-%s)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIMESTAMP.log"
-mkdir -p $LOGS_FOLDER
 
+#!/bin/bash
+userdata=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+mkdir -p $LOGS_FOLDER
 echo "hi"&>>$LOG_FILE
+
+
+
+
