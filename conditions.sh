3837adcc414af66187053741326534bb78d04533
naveen <naveenari117@gmail.com>
2024-08-28 10:24:18 +0530

    conditions

--- conditions.sh
+++ conditions.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 userid=$(id -u)
+
+
+validate()
+{
+ if [ $1 -ne 0 ]
+ then
+     echo "$2 is failed"
+
+ else
+    echo "$2 is success"  
+
+
+
+}
 if [ $userid -ne 0 ]
 then
     echo "plz choose root access"
     exit 1
 fi
-dnf install git -y    
+dnf install git -y  
+validate $? "git is"  
