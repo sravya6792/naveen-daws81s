afea463c92f307055664d80a71024ab720e6901b
naveen <naveenari117@gmail.com>
2024-08-28 10:28:57 +0530

    conditions

---
+++ loops.sh
@@ -0,0 +1,29 @@
+!/bin/bash
+userid=$(id -u)
+
+
+validate()
+{
+ if [ $1 -ne 0 ]
+ then
+     echo "$2 is failed"
+
+ else
+    echo "$2 is success"
+
+ fi     
+
+
+
+}
+if [ $userid -ne 0 ]
+then
+    echo "plz choose root access"
+    exit 1
+fi
+
+for package in $@
+do
+  dn install $package -y
+  validate $? "$package"
+done  
