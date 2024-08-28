bc0bb7a06b8b1fefb93283834eb403067dd5c0df
naveen <naveenari117@gmail.com>
2024-08-28 11:14:04 +0530

    conditions

--- nav.sh
+++ nav.sh
@@ -13,5 +13,33 @@ mkdir -p $LOGS_FOLDER
 echo "hi"&>>$LOG_FILE
 
 
+validate()
+{
+    if [ $? -ne 0 ]
+    then
+        echo  -e "$2....$R is not installed $N"| tee -a $LOG_FILE
+    else
+        echo  -e "$2...$G is installed  $N"| tee -a $LOG_FILE
+
+    fi        
+}
+echo "$userdata"
+if [ $userdata -ne 0 ]
+then
+   echo "plz choose root access"|tee -a $LOG_FILE
+   exit 1
+else
+   echo "$package installed"&>>LOG_FILE
+fi
+dnf install mysql-server -y
+validate $? "installing mysql server"
+
+systemctl enable mysqld
+validate $? "installing mysql server is enabled or not"
+systemctl start mysqld
+validate $? "installing mysql server is started or not"
+mysql_secure_installation --set-root-pass ExpenseApp@1
+validate $? "installing mysql server is spassword setup "
+
 
 
